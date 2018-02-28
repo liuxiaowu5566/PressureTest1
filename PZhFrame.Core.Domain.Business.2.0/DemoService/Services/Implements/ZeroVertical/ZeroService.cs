22309434037bbc5718d31e61ec888c0c49479bed
707114491 <admin@DESKTOP-81JDCL4>
2018-02-28 16:38:44 +0800

    修改t5_history查询方法

--- PZhFrame.Core.Domain.Business.2.0/DemoService/Services/Implements/ZeroVertical/ZeroService.cs
+++ PZhFrame.Core.Domain.Business.2.0/DemoService/Services/Implements/ZeroVertical/ZeroService.cs
@@ -4,12 +4,14 @@
 
 using DemoService.Services.Interface.Zero;
 using Models.Model;
+using Models.Model.t4;
 using PZhFrame.Data.DataService;
 using PZhFrame.ModelLayer.BaseModels;
 using PZhFrame.ModelLayer.Models.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace DemoService.Services.Implements.Zero
@@ -32,49 +34,32 @@ namespace DemoService.Services.Implements.Zero
         /// <returns></returns>
         public async Task<ApiResponse> GetHouse(int pageIndex, int pageSize)
         {
-            List<Result> list = new List<Result>();
-            List<t5_history> listHistory = new List<t5_history>();
-            List<t5_history> listResult = new List<t5_history>();
+            // 获取修改的列
+            List<t4_code> fileds = new t4_code().Select<t4_code>().ToList();
+            // 获取前9项实体属性
+            Type typeInfo = typeof(Result);
+            var properties = typeInfo.GetProperties().ToList();
             ApiResponse resp = new ApiResponse(null).OK();
-            try
+            //获取房子的初始信息
+            List<Result> List = new Result().SelectPart<Result>(typeof(t4_house).Name, pageIndex, pageSize, "column1");
+            //pageSize数进行循环,依次进行判断是否有修改
+            Parallel.ForEach(List, item =>
             {
-                string sqlStr = $@"select * from t4_house order by column1 OFFSET {(pageSize) * ((pageIndex) - 1)} ROW FETCH NEXT {pageSize} rows only";
-                list.AddRange(await dataService.GetAsync<Result>(sqlStr));
-                foreach (var item in list)
+                //根据houseid获取history的信息
+                List<t5_history> listHistory = new t5_history(true).Select<t5_history>(item.column1, "houseid");
+                //去处重复留下最新的记录
+                listHistory = (listHistory.Where((x, i) => listHistory.FindLastIndex(z => z.codeid == x.codeid) == i)).ToList();
+                Parallel.ForEach(listHistory, i =>
                 {
-                    string sqlHistory = $@"select * from t5_history where houseid = {item.column1}";
-                    listHistory.AddRange(await dataService.GetAsync<t5_history>(sqlHistory));
-                    listResult.AddRange(listHistory.Where((x, i) => listHistory.FindLastIndex(z => z.codeid == x.codeid) == i));
-                    foreach (var i in listResult)
+                    t4_code columnName = fileds.Where(o => o.id == i.codeid).FirstOrDefault();//判断code                    
+                    PropertyInfo proInfo = properties.Where(o => o.Name == columnName.name).FirstOrDefault();//判断code和house
+                    if (proInfo != null)
                     {
-                        switch (i.codeid)
-                        {
-                            case 1:
-                                item.column4 = i.value;
-                                break;
-                            case 2:
-                                item.column5 = i.value;
-                                break;
-                            case 3:
-                                item.column6 = i.value;
-                                break;
-                            case 4:
-                                item.column7 = i.value;
-                                break;
-                            case 5:
-                                item.column8 = i.value;
-                                break;
-                        }
+                        proInfo.SetValue(item, i.value);//添加新的信息
                     }
-                }
-            }
-            catch(Exception ex)
-            {
-                resp.Message = ex.Message;
-                resp = resp.SqlOperationExceptionResponse();
-            }
-            
-            resp.Model = list;
+                });
+            });
+            resp.Model = List;
             return resp;
         }
 
@@ -86,37 +71,27 @@ namespace DemoService.Services.Implements.Zero
         public async Task<ApiResponse> GetHouseInfo(string id)
         {
             List<t4_house> list = new List<t4_house>();
+            List<t4_code> fileds = new t4_code().Select<t4_code>().ToList();
+            Type typeInfo = typeof(t4_house);
+            var properties = typeInfo.GetProperties().ToList();
             List<t5_history> listHistory = new List<t5_history>();
-            List<t5_history> listResult = new List<t5_history>();
             ApiResponse resp = new ApiResponse(null).OK();
-            string sqlStr = $@"select * from t4_house where column2 = {id}";
+            string sqlStr = $@"select * from t4_house where column1 = {id}";
             list.AddRange(await dataService.GetAsync<t4_house>(sqlStr));
             foreach (var item in list)
             {
-                string sqlH = $@"select * from t5_history where houseid = {item.column2}";
-                listHistory.AddRange(await dataService.GetAsync<t5_history>(sqlH));
-                listResult.AddRange(listHistory.Where((x, i) => listHistory.FindLastIndex(z => z.codeid == x.codeid) == i));
-                foreach (var i in listResult)
+                string sqlH = $@"select * from t5_history where houseid = {item.column1}";
+                listHistory = (await dataService.GetAsync<t5_history>(sqlH)).ToList();
+                listHistory = (listHistory.Where((x, i) => listHistory.FindLastIndex(z => z.codeid == x.codeid) == i)).ToList();
+                Parallel.ForEach(listHistory, i =>
                 {
-                    switch (i.codeid)
+                    t4_code columnName = fileds.Where(o => o.id == i.codeid).FirstOrDefault();
+                    PropertyInfo proInfo = properties.Where(o => o.Name == columnName.name).FirstOrDefault();
+                    if (proInfo != null)
                     {
-                        case 1:
-                            item.column4 = i.value;
-                            break;
-                        case 2:
-                            item.column5 = i.value;
-                            break;
-                        case 3:
-                            item.column6 = i.value;
-                            break;
-                        case 4:
-                            item.column7 = i.value;
-                            break;
-                        case 5:
-                            item.column8 = i.value;
-                            break;
+                        proInfo.SetValue(item, i.value);
                     }
-                }
+                });
             }
             resp.Model = list;
             return resp;
