18d03de0bb2cc56ef38b763cb46a71737e35316f
lyl <liuwuwu66@gmail.com>
2018-03-01 11:56:34 +0800

    hq

--- PZhFrame.Core.Domain.Business.2.0/DemoService/Services/Data/PreparationService.cs
+++ PZhFrame.Core.Domain.Business.2.0/DemoService/Services/Data/PreparationService.cs
@@ -1,14 +1,29 @@
 ﻿
 using Models.Model;
 using Models.Model.t2;
+using PZhFrame.Data.DataService;
+using PZhFrame.ModelLayer.BaseModels;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace DemoService.Services.Data
 {
     public class PreparationService
     {
+        IDataService dataService = null;
+        static ConnectionStringsHelper connection = new ConnectionStringsHelper();
+        public PreparationService()
+        {
+            dataService = new DataService(connection.ConnString(), connection.SqlType());
+        }
+
+        public async Task<ResponseModel<t2_house>> SelectMatch()
+        {
+            return null;
+            return new ResponseModel<t2_house>(null);
+        }
         public int Add(t2_house model)
         {
             return model.Insert();
---
+++ PZhFrame.Core.Domain.Business.2.0/DemoTest/DataAdd/SpeedTest.cs
@@ -0,0 +1,20 @@
+﻿using DemoService.Services.Data;
+using System.Diagnostics;
+using Xunit;
+
+namespace DemoTest.DataAdd
+{
+    public class SpeedTest
+    {
+        [Fact]
+        public void SelectTest()
+        {
+            Stopwatch sp = new Stopwatch();
+            sp.Start();
+            PreparationService service = new PreparationService();
+            var result = service.SelectMatch().Result;
+            sp.Stop();
+            var time = sp.Elapsed;
+        }
+    }
+}
