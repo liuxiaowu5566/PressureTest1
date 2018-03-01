982b418c2fb3bd2a2870dbf62cbb19ea396175cf
707114491 <admin@DESKTOP-81JDCL4>
2018-03-01 16:25:59 +0800

    修改返回类型

--- PZhFrame.Core.Domain.Business.2.0/DemoService/Services/Implements/ZeroVertical/ZeroService.cs
+++ PZhFrame.Core.Domain.Business.2.0/DemoService/Services/Implements/ZeroVertical/ZeroService.cs
@@ -9,6 +9,7 @@ using PZhFrame.Data.DataService;
 using PZhFrame.ModelLayer.BaseModels;
 using PZhFrame.ModelLayer.Models.Models;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -32,35 +33,28 @@ namespace DemoService.Services.Implements.Zero
         /// <param name="index"></param>
         /// <param name="pageSize"></param>
         /// <returns></returns>
-        public async Task<ApiResponse> GetHouse(int pageIndex, int pageSize)
+        public ResponseModel<Result> GetHouse(int pageIndex, int pageSize)
         {
-            // 获取修改的列
             List<t4_code> fileds = new t4_code().Select<t4_code>().ToList();
-            // 获取前9项实体属性
             Type typeInfo = typeof(Result);
             var properties = typeInfo.GetProperties().ToList();
-            ApiResponse resp = new ApiResponse(null).OK();
-            //获取房子的初始信息
             List<Result> List = new Result().SelectPart<Result>(typeof(t4_house).Name, pageIndex, pageSize, "column1");
-            //pageSize数进行循环,依次进行判断是否有修改
             Parallel.ForEach(List, item =>
-            {
-                //根据houseid获取history的信息
-                List<t5_history> listHistory = new t5_history(true).Select<t5_history>(item.column1, "houseid");
-                //去处重复留下最新的记录
-                listHistory = (listHistory.Where((x, i) => listHistory.FindLastIndex(z => z.codeid == x.codeid) == i)).ToList();
-                Parallel.ForEach(listHistory, i =>
                 {
-                    t4_code columnName = fileds.Where(o => o.id == i.codeid).FirstOrDefault();//判断code                    
-                    PropertyInfo proInfo = properties.Where(o => o.Name == columnName.name).FirstOrDefault();//判断code和house
-                    if (proInfo != null)
-                    {
-                        proInfo.SetValue(item, i.value);//添加新的信息
-                    }
+                    List<t5_history> listHistory = new t5_history(true).Select<t5_history>(item.column1, "houseid");
+                    listHistory = (listHistory.Where((x, i) => listHistory.FindLastIndex(z => z.codeid == x.codeid) == i)).ToList();
+                    Parallel.ForEach(listHistory, i =>
+                        {
+                            t4_code columnName = fileds.Where(o => o.id == i.codeid).FirstOrDefault();
+                            PropertyInfo proInfo = properties.Where(o => o.Name == columnName.name).FirstOrDefault();
+                            if (proInfo != null)
+                            {
+                                proInfo.SetValue(item, i.value);
+                            }
+                        });
                 });
-            });
-            resp.Model = List;
-            return resp;
+            ResponseModel<Result> resModel = new ResponseModel<Result>(List);
+            return resModel;
         }
 
         /// <summary>
--- PZhFrame.Core.Domain.Business.2.0/DemoTest/ZeroVertical/ZeroText.cs
+++ PZhFrame.Core.Domain.Business.2.0/DemoTest/ZeroVertical/ZeroText.cs
@@ -2,6 +2,7 @@
 using DemoService.Services.Interface.Zero;
 using Models.Model;
 using PZhFrame.Data.DataService;
+using PZhFrame.ModelLayer.Models.Models;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -21,17 +22,27 @@ namespace DemoTest.Zero
         [Fact]
         public async void QueryPageTime()
         {
-            List<TimeSpan> listTime = new List<TimeSpan>();
-            List<ApiResponse> result = new List<ApiResponse>();
-            for (int i = 1; i < 500; i = i + 100)
-            { 
+            List<double> listResult = new List<double>();
+            List<double> listTime = new List<double>();
+            //List<ResponseModel<t4_house>> result = new List<ResponseModel<t4_house>>();
+            //for (int i = 1; i < 11; i = i + 10)
+            //{
+            //    Stopwatch sw = new Stopwatch();
+            //    sw.Start();
+            //    result.Add(await zeroService.GetHouse(i, 15));
+            //    sw.Stop();
+            //    listTime.Add(sw.Elapsed);
+            //}
+            List<ResponseModel<Result>> result = new List<ResponseModel<Result>>();
+            for (int i = 1; i < 101; i++)
+            {
                 Stopwatch sw = new Stopwatch();
                 sw.Start();
-                result.Add(await zeroService.GetHouse(i, 15));
+                result.Add(zeroService.GetHouse(i, 15));
                 sw.Stop();
-                listTime.Add(sw.Elapsed);
+                listTime.Add(sw.Elapsed.TotalMilliseconds);
             }
+            listResult.AddRange(listTime);
         }
-
     }
 }
