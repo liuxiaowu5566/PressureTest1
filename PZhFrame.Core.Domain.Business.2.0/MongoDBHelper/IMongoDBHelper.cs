4ad4b4c62d78fc05924594dfe60ae4bf949ac815
sunjingwu <954547162@qq.com>
2018-03-06 18:33:57 +0800

    修改

--- PZhFrame.Core.Domain.Business.2.0/MongoDBHelper/IMongoDBHelper.cs
+++ PZhFrame.Core.Domain.Business.2.0/MongoDBHelper/IMongoDBHelper.cs
@@ -1,8 +1,11 @@
-﻿using MongoDB.Driver;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq.Expressions;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace MongoDBHelper
 {
@@ -39,18 +42,18 @@ namespace MongoDBHelper
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="filter"></param>
-        /// <param name="pageIndex"></param>
-        /// <param name="pageSize"></param>
         /// <returns></returns>
-        List<T> Select<T>(Expression<Func<T, bool>> filter, int pageIndex=1, int pageSize= 15, string oderbyFiled = "_id");
+        Task<List<T>> SelectAsync<T>(Expression<Func<T, bool>> filter);
 
         /// <summary>
         /// 
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="filter"></param>
+        /// <param name="pageIndex"></param>
+        /// <param name="pageSize"></param>
         /// <returns></returns>
-        long Delete<T>(Expression<Func<T, bool>> filter);
+        List<T> Select<T>(Expression<Func<T, bool>> filter, int pageIndex=1, int pageSize= 15, string oderbyFiled = "_id");
 
         /// <summary>
         /// 
@@ -64,10 +67,92 @@ namespace MongoDBHelper
         /// <summary>
         /// 
         /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="filter"></param>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        Task<long> UpdateAsync<T>(Expression<Func<T, bool>> filter, T model);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="filter"></param>
+        /// <returns></returns>
+        long Delete<T>(Expression<Func<T, bool>> filter);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        Task<long> DeleteAsync<T>(Expression<Func<T, bool>> filter);
+
+        /// <summary>
+        /// 
+        /// </summary>
         /// <typeparam name="TInput"></typeparam>
         /// <typeparam name="TOutput"></typeparam>
         /// <param name="jsonJoinStr"></param>
         /// <returns></returns>
         List<TOutput> Join<TInput, TOutput>(string jsonJoinStr);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="fileData"></param>
+        /// <returns></returns>
+        ObjectId UpLoad(string fileName, byte[] fileData);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="fileData"></param>
+        /// <returns></returns>
+        Task<ObjectId> UpLoadAsync(string fileName, byte[] fileData);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="fileStream"></param>s
+        /// <returns></returns>
+        ObjectId UpLoad(string fileName, Stream fileStream);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="fileStream"></param>
+        /// <returns></returns>
+        Task<ObjectId> UpLoadAsync(string fileName, Stream fileStream);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        byte[] DownLoad(string fileName);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        Task<byte[]> DownLoadAsync(string fileName);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fileId"></param>
+        /// <returns></returns>
+        byte[] DownLoad(ObjectId fileId);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fileId"></param>
+        /// <returns></returns>
+        Task<byte[]> DownLoadAsync(ObjectId fileId);
     }
 }
